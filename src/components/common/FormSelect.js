5faad9d7e43b5c46965a5b9e62e76811b2ad53b1
Sharma <parul.s.sharma@kronos.com>
2020-09-01 19:49:37 +0530

    adding new invoive page functionality

--- src/components/common/FormSelect.js
+++ src/components/common/FormSelect.js
@@ -1,22 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Form, Col, Row, Button } from 'react-bootstrap';
-import { BASE_URL, REST_API } from '../Constants';
-import PageHeading from './common/PageHeading';
+import React from 'react';
+import { Form, Col, Row } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 
+FormSelect.propTypes = {
+    label: PropTypes.string,
+    dropdDownList: PropTypes.array.isRequired
+}
+export default function FormSelect({ label, dropdDownList, placeholder, onChange }) {
 
-export default function FormSelect() {
-    return(
-        <Form.Group as={Row} controlId="exampleForm.ControlSelect1">
-        <Form.Label>Example select</Form.Label>
-        <Col>
-        <Form.Control as="select">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-            </Form.Control>
-        </Col>
-        </Form.Group>
+    return (
+        <>
+            {dropdDownList &&
+                <Form.Group as={Row} controlId="exampleForm.ControlSelect1">
+                    <Form.Label>{label}</Form.Label>
+                    <Col>
+                        <Form.Control as="select" onChange={onChange}>
+                            <option value="" key={placeholder}> {placeholder} </option>
+                            {dropdDownList.map((data) =>
+                                <option value={data.id} key={data} >
+                                    {data.name}
+                                </option>)}
+
+                        </Form.Control>
+                    </Col>
+                </Form.Group>
+            }
+        </>
     )
 }
--- src/components/common/TableView.js
+++ src/components/common/TableView.js
@@ -1,44 +1,44 @@
 import React from 'react';
 import { Table } from 'react-bootstrap';
-import { FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowAltCircleDown, faTrashAlt, faEdit } from '@fortawesome/free-regular-svg-icons';
 
 
 function TableView(props) {
     return (
         <div>
-        <Table striped bordered hover size="sm" responsive >
-            <thead>
-                <tr>
-                {props.header.map((element, index) =>
-                        <th key={index}>{element}</th>
-                    )}
-              {props.download?<th></th>:<></>}  
-              {props.delete?<th></th>:<></>}
-              {props.edit?<th></th>:<></>}  
-                        
+            <Table striped bordered hover size="sm" responsive >
+                <thead>
+                    <tr>
+                        {props.header.map((element, index) =>
+                            <th key={index}>{element}</th>
+                        )}
+                        {props.download ? <th></th> : <></>}
+                        {props.delete ? <th></th> : <></>}
+                        {props.edit ? <th></th> : <></>}
 
-                </tr>
-            </thead>
-            <tbody>
-                {props.body.map((element, index) =>
-                <tr key={index}>
-                     
-                   { Object.keys(element).map((key, index) => 
-                    key!=='id'?<td key={key+'_'+index}>
-                        {element[key]}
-                    </td>:<></>
-                   )}
-                   {props.download?<td><FontAwesomeIcon icon={faArrowAltCircleDown} size="lg" onClick={() =>props.download(element.id, element[props.downloadFileName])}/></td>
-                      :<></>}
-                     {props.delete?<td><FontAwesomeIcon icon={faTrashAlt} size="lg" onClick={() =>props.delete(element.id)}/></td>
-                     :<></>}
-                     {props.edit?<td><FontAwesomeIcon icon={faEdit} size="lg" onClick={() =>props.edit(element.id)}/></td>
-                     :<></>}
-                </tr>
-                )}
-            </tbody>
-        </Table>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.body.map((element, index) =>
+                        <tr key={index}>
+
+                            {Object.keys(element).map((key, index) =>
+                                key !== 'id' ? <td key={key + '_' + index}>
+                                    {element[key]}
+                                </td> : <></>
+                            )}
+                            {props.download ? <td><FontAwesomeIcon icon={faArrowAltCircleDown} size="lg" onClick={() => props.download(element.id, element[props.downloadFileName])} /></td>
+                                : <></>}
+                            {props.delete ? <td><FontAwesomeIcon icon={faTrashAlt} size="lg" onClick={() => props.delete(element.id)} /></td>
+                                : <></>}
+                            {props.edit ? <td><FontAwesomeIcon icon={faEdit} size="lg" onClick={() => props.edit(element.id)} /></td>
+                                : <></>}
+                        </tr>
+                    )}
+                </tbody>
+            </Table>
         </div>
     )
 }
